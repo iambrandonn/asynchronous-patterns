cc87bdc5f3a099293ff38066da548f3206f2ef08
Brandon Nicholls <brandon.nicholls@gmail.com>
2017-11-07 10:51:19 -0700

    Adds bluebird and native promisify examples

---
+++ calculate-product-total-bluebird.js
@@ -0,0 +1,63 @@
+const bluebird = require('bluebird')
+
+function makeDatabaseCallCallback (valueToFind, callback) {
+  const delay = 3000
+  setTimeout(() => {
+    switch (valueToFind) {
+      case 'Cost':
+        callback(null, 100)
+        break
+      case 'Tax':
+        callback(null, 15)
+        break
+      case 'Discount':
+        callback(null, 10)
+        break
+      default:
+        callback(new Error(`invalid valueToFind argument: ${valueToFind}`))
+    }
+  }, delay)
+}
+
+const makeDatabaseCall = bluebird.promisify(makeDatabaseCallCallback)
+
+function getCostInfo () {
+  return makeDatabaseCall('Cost')
+}
+
+function getTaxInfo () {
+  return makeDatabaseCall('Tax')
+}
+
+function getDiscountInfo () {
+  return makeDatabaseCall('Discount')
+}
+
+let total = 0
+let cost
+
+let startTime = Date.now()
+
+getCostInfo()
+  .then(value => {
+    cost = value
+    total = cost
+    console.log(`total is ${total}`)
+    return getTaxInfo()
+  })
+  .then(tax => {
+    total = total + tax / 100 * cost
+    console.log(`total is ${total}`)
+    return getDiscountInfo()
+  })
+  .then(discount => {
+    discount = discount / 100 * total
+    total = total - discount
+    console.log(`total is ${total}`)
+  })
+  .then(() => {
+    console.log(`${(Date.now() - startTime) / 1000} seconds to complete`)
+  })
+  .catch(err => {
+    console.error(err.message)
+  })
