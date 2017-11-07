54c2a62dafaa4b72d603872cfa60bc256a100169
Brandon Nicholls <brandon.nicholls@gmail.com>
2017-11-07 10:53:24 -0700

    Promise.all using promisify for simplicity

--- calculate-product-total-promise-all.js
+++ calculate-product-total-promise-all.js
@@ -1,40 +1,50 @@
-function makeDatabaseCall (valueToFind, resolve) {
+const util = require('util')
+
+function makeDatabaseCallCallback (valueToFind, callback) {
+  const delay = 3000
   setTimeout(() => {
-    let value = valueToFind === 'Cost' ? 100 : valueToFind === 'Tax' ? 15 : 10
-    resolve(value)
-  }, 3000)
+    switch (valueToFind) {
+      case 'Cost':
+        callback(null, 100)
+        break
+      case 'Tax':
+        callback(null, 15)
+        break
+      case 'Discount':
+        callback(null, 10)
+        break
+      default:
+        callback(new Error(`invalid valueToFind argument: ${valueToFind}`))
+    }
+  }, delay)
 }
 
+const makeDatabaseCall = util.promisify(makeDatabaseCallCallback)
+
 function getCostInfo () {
-  return new Promise((resolve, reject) => {
-    makeDatabaseCall('Cost', resolve)
-  })
+  return makeDatabaseCall('Cost')
 }
 
 function getTaxInfo () {
-  return new Promise((resolve, reject) => {
-    makeDatabaseCall('Tax', resolve)
-  })
+  return makeDatabaseCall('Tax')
 }
 
 function getDiscountInfo () {
-  return new Promise((resolve, reject) => {
-    makeDatabaseCall('Discount', resolve)
-  })
+  return makeDatabaseCall('Discount')
 }
 
 let startTime = Date.now()
 
-Promise.all([
-  getCostInfo(),
-  getTaxInfo(),
-  getDiscountInfo()
-]).then(([cost, tax, discount]) => {
-  let total = cost
-  total = total + tax / 100 * cost
-  discount = discount / 100 * total
-  total = total - discount
-  console.log(`total is ${total}`)
+Promise.all([getCostInfo(), getTaxInfo(), getDiscountInfo()])
+  .then(([cost, tax, discount]) => {
+    let total = cost
+    total = total + tax / 100 * cost
+    discount = discount / 100 * total
+    total = total - discount
+    console.log(`total is ${total}`)
 
-  console.log(`${(Date.now() - startTime) / 1000} seconds to complete`)
-})
+    console.log(`${(Date.now() - startTime) / 1000} seconds to complete`)
+  })
+  .catch(err => {
+    console.error(err.message)
+  })
